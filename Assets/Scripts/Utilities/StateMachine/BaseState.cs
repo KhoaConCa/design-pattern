96d2e712ca97739ba36579d21c8acf6efca813d5
KhoaDN <nhatkhoa.working@gmail.com>
2025-06-16 15:05:24 +0700

    Create map, backgound, state machine abstract class

---
+++ Assets/Scripts/Utilities/StateMachine/BaseState.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+namespace TurnBase2D.Utilities.StateMachine
+{
+    /// <summary>
+    /// BaseState<EState> - Là một abstract class đại diện cho một state trong State Machine.
+    /// Tác giả: Dương Nhật Khoa - Ngày tạo: 16/06/2025.
+    /// </summary>
+    /// <typeparam name="EState">State của đối tượng được biểu diễn bằng Enum</typeparam>
+    public abstract class BaseState<EState> where EState : Enum
+    {
+        #region --- Contructor ---
+
+        public BaseState(EState key)
+        {
+            StateKey = key;
+        }
+
+        #endregion
+
+        #region --- Abstract Methods ---
+
+        public abstract void EnterState();
+        public abstract void ExitState();
+        public abstract void UpdateState();
+        public abstract EState GetNextState();
+        public abstract void OnTriggerEnter(Collider other);
+        public abstract void OnTriggerStay(Collider other);
+        public abstract void OnTriggerExit(Collider other);
+
+        #endregion
+
+        #region --- Properties ---
+
+        public EState StateKey { get; private set; }
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Utilities/StateMachine/StateManager.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+namespace TurnBase2D.Utilities.StateMachine
+{
+    /// <summary>
+    /// StateManager<EState> - Là một abstract class đại diện cho quản lý state trong State Machine.
+    /// Tác giả: Dương Nhật Khoa - Ngày tạo: 16/06/2025.
+    /// </summary>
+    /// <typeparam name="EState">Các state của đối tượng được viết dưới dạng Enum</typeparam>
+    public abstract class StateManager<EState> : MonoBehaviour where EState : Enum
+    {
+        #region --- Unity Methods ---
+
+        void Start()
+        {
+            currentState.EnterState();
+        }
+
+        void Update()
+        {
+            EState nextStateKey = currentState.GetNextState();
+
+            if (!isTransitioningState && nextStateKey.Equals(currentState.StateKey))
+            {
+                currentState.UpdateState();
+            }
+            else
+            {
+                TransitionToState(nextStateKey);
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            currentState.OnTriggerEnter(other);
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            currentState.OnTriggerStay(other);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            currentState.OnTriggerExit(other);
+        }
+
+        #endregion
+
+        #region --- Methods ---
+
+        private void TransitionToState(EState nextStateKey)
+        {
+            isTransitioningState = true;
+
+            currentState.ExitState();
+            currentState = states[nextStateKey];
+            currentState.EnterState();
+
+            isTransitioningState = false;
+        }
+
+        #endregion
+
+        #region --- Fields ---
+
+        protected Dictionary<EState, BaseState<EState>> states = new Dictionary<EState, BaseState<EState>>();
+        protected BaseState<EState> currentState;
+
+        protected bool isTransitioningState = false;
+
+        #endregion
+    }
+}
